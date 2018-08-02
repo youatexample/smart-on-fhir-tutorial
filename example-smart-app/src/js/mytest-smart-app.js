b098d163f29276f0ea12d102a701fca9e4dddee7
youatexample <36056237+youatexample@users.noreply.github.com>
2018-08-02 14:16:40 -0400

    Create mytest-smart-app.js

---
+++ example-smart-app/src/js/mytest-smart-app.js
@@ -0,0 +1,12 @@
+(function(window){
+  window.extractData = function() {
+    var ret = $.Deferred();
+
+    function onError() {
+      console.log('Loading error', arguments);
+      ret.reject();
+    }
+    
+    //TODO get some other resource
+    
+})(window);
