bd2aecca9b1c3db0229151d58da769ccaafdc117
youatexample <36056237+youatexample@users.noreply.github.com>
2018-08-08 19:06:54 -0400

    Update mytest-smart-app.js

--- example-smart-app/src/js/mytest-smart-app.js
+++ example-smart-app/src/js/mytest-smart-app.js
@@ -6,7 +6,53 @@
       console.log('Loading error', arguments);
       ret.reject();
     }
+
+    function onReady(smart)  {
+      if (smart.hasOwnProperty('patient')) {
+        var patient = smart.patient;
+        var pt = patient.read();
+        var meds = smart.patient.api.search({type: 'MedicationRequest'});
+
+        $.when(pt, meds).fail(onError);
+
+        $.when(pt, meds).done(function(patient, meds) {
+          var fullName = '';
+          var fname = '';
+          var lname = '';
+
+          if (typeof patient.name[0] !== 'undefined') {
+            fname = patient.name[0].given.join(' ');
+            lname = patient.name[0].family.join(' ');            
+          }          
+
+          var p = defaultPatient();
+          p.fullName = fname + lname;
+          p.meds = meds[0].medication;
+          ret.resolve(p);
+        });
+      } else {
+        onError();
+      }
+    }
+
+    FHIR.oauth2.ready(onReady, onError);
+    return ret.promise();
+
+  };
+
+  function defaultPatient(){
+    return {
+      fullName: {value: ''},
+      meds: {value: ''}      
+    };
+  }
+
+  
+  window.drawVisualization = function(p) {
+    $('#holder').show();
+    $('#loading').hide();
+    $('#fullname').html(p.fullName);
     
-    //TODO get some other resource
-    
+  };
+
 })(window);
