a5d098800a71e594bbfa2ea7bd997fea6bb1ee3f
youatexample <36056237+youatexample@users.noreply.github.com>
2018-08-10 21:54:57 -0400

    Update mytest-smart-app.js

--- example-smart-app/src/js/mytest-smart-app.js
+++ example-smart-app/src/js/mytest-smart-app.js
@@ -11,7 +11,7 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
-        //var meds = smart.patient.api.fetchAll({type: 'MedicationOrder'});
+        var meds = smart.patient.api.search({type: 'MedicationOrder'});
 
         $.when(pt).fail(onError);
 
